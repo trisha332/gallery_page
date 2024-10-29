6c7ba182396f9f69d81f0ea2bd4b46318c9e5050
trisha332 <mondaltrisha432@gmail.com>
2024-10-29 09:57:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- lightbox -->
+     <link rel="stylesheet" href="lightbox2-dev/dist/css/lightbox.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+include('../fixed_navber/index1.php');
+
+
+ ?>
+
+<div class="grid-gallery">
+    <div class="grid-item">
+        <a href="images/image1.jpeg"  data-lightbox="gridImage" >
+            <img src="images/image1.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image2.jpeg  "  data-lightbox="gridImage" >
+            <img src="images/image2.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image3.png " data-lightbox="gridImage" >
+            <img src="images/image3.png" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image4.jpeg"  data-lightbox="gridImage" >
+            <img src="images/image4.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image5.jpeg"  data-lightbox="gridImage" >
+            <img src="images/image5.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image6.jpeg"  data-lightbox="gridImage" >
+            <img src="images/image6.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image7.jpeg"  data-lightbox="gridImage" >
+            <img src="images/image7.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image8.jpeg"  data-lightbox="gridImage" >
+            <img src="images/image8.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image9.jpeg" data-lightbox="gridImage" >
+            <img src="images/image9.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image10.png" data-lightbox="gridImage" >
+            <img src="images/image10.png" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image11.png" data-lightbox="gridImage" >
+            <img src="images/image11.png" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image19.jpeg" data-lightbox="gridImage" >
+            <img src="images/image19.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image13.jpeg" data-lightbox="gridImage" >
+            <img src="images/image13.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image24.jpeg" data-lightbox="gridImage" >
+            <img src="images/image24.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image15.jpeg" data-lightbox="gridImage" >
+            <img src="images/image15.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image16.jpeg" data-lightbox="gridImage" >
+            <img src="images/image16.jpeg" alt="">
+        </a>
+    </div>
+    <div class="grid-item">
+        <a href="images/image17.jpeg " data-lightbox="gridImage" >
+            <img src="images/image17.jpeg" alt="">
+        </a>
+    </div>
+
+    <div class="grid-item">
+        <a href="images/image18.jpeg " data-lightbox="gridImage" >
+            <img src="images/image18.jpeg" alt="">
+        </a>
+    </div>
+
+    
+
+    
+
+    
+    
+</div>
+
+<!-- footer -->
+<footer class="footer-distributed">
+    <div class="footer-left">
+        <h2>Soner <span>Hasel</span></h2>
+   
+    <p class="footer-links">
+        <a href="../slider/index.php">Home</a>
+        |
+        <a href="../about us/index.php">About</a>
+        |
+        <a href="../contact us/index.php">Contact</a>
+        
+        
+    </p>
+    <p class="footer-company-name">Copyright © 2021 <strong>Soner Hasel</strong> All rights reserved </p>
+</div>
+<div class="footer-center">
+    <div>
+        <i class="fa fa-phone"></i>
+        <p>+91 78421785825</p>
+    </div>
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="#">sonerhasel78@gmail.com</a></p>
+    </div>
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="#">facebook</a></p>
+    </div>
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="#">instagram</a></p>
+    </div>
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="#">Twiter</a></p>
+    </div>
+</div>
+    <div class="footer-right">
+        <p class="footer-company-about">
+           <span>About the company</span>
+           <strong>Soner Hasel</strong> is authentic Bengali Restaurant where you can find authentic Bengali food item.</p>
+        <!-- <div class="footer-icons">
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        
+
+       
+
+    </div> -->
+
+    <div class="address"style="color:white"><br><b>address</b><br>
+            <a href="#" style="color:white" > charabagan, near  dumdum metro station pin-700050;</a>
+    </div>
+</div>
+    
+</footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <!--  lightbox js-->
+     <script src="lightbox2-dev/dist/js/lightbox-plus-jquery.min.js"></script>
+    
+     <script src="script.js"></script>
+
+</body>
+</html>
